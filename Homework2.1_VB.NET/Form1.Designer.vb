d7009f88145848e4ef073d8429cc7aa09b05a591
Alberto Calabrese <calabrese.1709532@studenti.uniroma1.it>
2021-10-08 10:20:38 +0200

    Added Online Mean Algorithm

--- Homework2.1_VB.NET/Form1.Designer.vb
+++ Homework2.1_VB.NET/Form1.Designer.vb
@@ -22,10 +22,59 @@ Partial Class Form1
     'Non modificarla mediante l'editor del codice.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(803, 391)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 397)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 41)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(655, 397)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 41)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Homework2.1_VB.NET/Form1.vb
+++ Homework2.1_VB.NET/Form1.vb
@@ -1,3 +1,43 @@
 ﻿Public Class Form1
+    Private random As Random
+    Private timer As Timer
+    Public Property mean As Double
+    Public Property n As Integer
 
+    Public Sub New()
+        InitializeComponent()
+        random = New Random()
+        timer = New Timer()
+
+        timer.Enabled = True
+        AddHandler timer.Tick, AddressOf Timer1_Tick
+
+        timer.Interval = 400
+        timer.Stop()
+
+        mean = 0
+        n = 0
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        timer.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        timer.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim generatedValue As Integer = random.[Next](18, 31)
+        updateMean(generatedValue)
+        Me.TextBox1.AppendText("Generated Number: " & generatedValue)
+        Me.TextBox1.AppendText(Environment.NewLine)
+        Me.TextBox1.AppendText("New mean = " & mean.ToString())
+        Me.TextBox1.AppendText(Environment.NewLine)
+    End Sub
+
+    Private Sub updateMean(ByVal nextNumber As Integer)
+        n += 1
+        mean = mean + ((1D / n) * (nextNumber - mean))
+    End Sub
 End Class
